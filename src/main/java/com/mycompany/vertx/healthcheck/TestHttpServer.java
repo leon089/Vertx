05fa5935d8a991ca2243165811cc53fb668947ad
Lihua Gao <lihua.gao@deutschebahn.com>
2022-09-19 13:53:03 +0200

    healthcheck-handler, positive/negative

--- src/main/java/com/mycompany/vertx/healthcheck/TestHttpServer.java
+++ src/main/java/com/mycompany/vertx/healthcheck/TestHttpServer.java
@@ -11,33 +11,46 @@ import io.vertx.ext.healthchecks.HealthChecks;
 import io.vertx.ext.healthchecks.Status;
 import io.vertx.ext.web.Router;
 import io.vertx.core.AbstractVerticle;
+
 public class TestHttpServer extends AbstractVerticle {
 
-    public static void main(String[] args) {
-        Vertx vertx = Vertx.vertx();
-        vertx.deployVerticle(new TestHttpServer());
-    }
+  public static void main(String[] args) {
+    Vertx vertx = Vertx.vertx();
+    vertx.deployVerticle(new TestHttpServer());
+  }
 
-    @Override
-    public void start() throws Exception {
-      // HealthChecks hc = HealthChecks.create(vertx);
+  @Override
+  public void start() throws Exception {
+    // HealthChecks hc = HealthChecks.create(vertx);
 
-      // hc.register(
-      //   "my-procedure",
-      //   promise -> promise.complete(Status.OK()));
-        
-      Router router = Router.router(vertx);
-  
-      HealthCheckHandler healthCheckHandler1 = HealthCheckHandler.create(vertx);
+    // hc.register(
+    // "my-procedure",
+    // promise -> promise.complete(Status.OK()));
 
-      router.get("/health*").handler(healthCheckHandler1);
+    Router router = Router.router(vertx);
 
+    HealthCheckHandler healthCheckHandler1 = HealthCheckHandler.create(vertx);
 
-      router.route().handler(routingContext -> {
-        routingContext.response().putHeader("content-type", "text/html").end("Hello World!");
-      });
-  
-      vertx.createHttpServer().requestHandler(router).listen(8080);
-    }
+    // Register procedures
+    // It can be done after the route registration, or even at runtime
+    healthCheckHandler1.register("my-procedure-name", promise -> {
+      // Do the check ....
+      boolean ok = true;
+      // Upon success do
+      if (ok)
+        promise.complete(Status.OK());
+      // In case of failure do:
+      else
+        promise.complete(Status.KO());
+    });
+
+    router.get("/health*").handler(healthCheckHandler1);
+
+    router.route().handler(routingContext -> {
+      routingContext.response().putHeader("content-type", "text/html").end("Hello World!");
+    });
+
+    vertx.createHttpServer().requestHandler(router).listen(8080);
+  }
 
 }
